818d3c678d172eb1742e72101ef370029d265ff3
Vlad <bahlaiv170307@gmail.com>
2026-07-02 20:16:41 +0300

    feat: cart models and admin

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    search_fields = ['user__username',]
+    list_display = ['user', 'created_at']
+    ordering = ['-created_at',]
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    search_fields = ['cart__user__username', 'variant__product__name']
+    list_filter = ['variant__size',]
+    list_display = ['cart', 'variant', 'quantity']
+    ordering = ['-quantity',]
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from users.models import CustomUser
+from inventory.models import ProductVariant
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        related_name='cart'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Створено')
+
+
+    class Meta:
+        ordering = ['-created_at',]
+        verbose_name = 'Кошик'
+        verbose_name_plural = 'Кошики'
+
+    def __str__(self):
+        return f'Кошик: {self.user}'
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart,
+        on_delete=models.CASCADE,
+        related_name='cart_item'
+    )
+
+    variant = models.ForeignKey(
+        ProductVariant,
+        on_delete=models.CASCADE,
+        related_name='cart_variant'
+    )
+
+    quantity = models.PositiveIntegerField(verbose_name='Кількість')
+
+
+    class Meta:
+        ordering = ['-quantity',]
+        unique_together = ['cart', 'variant']
+        verbose_name = 'Замовлення в кошику'
+        verbose_name_plural = 'Замовлення в кошиках'
+
+
+    def __str__(self):
+        return f'{self.cart}: {self.variant}'
