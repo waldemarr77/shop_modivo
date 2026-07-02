e3a43e5c92ab439b433de355ac3507e451c567f8
Vlad <bahlaiv170307@gmail.com>
2026-07-02 17:01:54 +0300

    feat: inventory models and admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Size, ProductVariant
 
-# Register your models here.
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+    search_fields = ['name',]
+    list_display = ['name',]
+    ordering = ['-name',]
+
+
+@admin.register(ProductVariant)
+class ProductVariantAdmin(admin.ModelAdmin):
+    search_fields = ['product__name', 'size__name']
+    list_filter = ['product__name', 'size__name']
+    list_display = ['product', 'size', 'stock', 'sku']
+    ordering = ['stock',]
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from catalog.models import Product
 
-# Create your models here.
+class Size(models.Model):
+    name = models.CharField(max_length=10, verbose_name='Розмір')
+
+    class Meta:
+        ordering = ['-name',]
+        verbose_name = 'Розмір'
+        verbose_name_plural = 'Розміри'
+
+    def __str__(self):
+        return self.name
+    
+
+class ProductVariant(models.Model):
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        related_name='variants'
+    )
+
+    size = models.ForeignKey(
+        Size,
+        on_delete=models.CASCADE,
+        related_name='size_product'
+    )
+
+    stock = models.PositiveIntegerField(verbose_name='Кількість товару на складі')
+    sku = models.CharField(max_length=20, unique=True, verbose_name='Артикул товару')
+
+
+    class Meta:
+        ordering = ['-stock',]
+        unique_together = ['product', 'size']
+        verbose_name = 'Варіант товару'
+        verbose_name_plural = 'Варіанти товарів'
+
+    def __str__(self):
+        return f'{self.product} size {self.size} - {self.sku}'
