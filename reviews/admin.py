fc767ea746f68717bd4c38cff529de3da857e9f6
Vlad <bahlaiv170307@gmail.com>
2026-07-06 18:13:43 +0300

    feat: reviews models and admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    search_fields = ['user__username', 'product__name']
+    list_filter = ['rating',]
+    list_display = ['user', 'product', 'rating', 'created_at']
+    ordering = ['-created_at',]
--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from users.models import CustomUser
+from catalog.models import Product
 
-# Create your models here.
+class Review(models.Model):
+    CHOICES_RATING = [
+        (1, 'Погано'),
+        (2, 'Незадовільно'),
+        (3, 'Задовільно'),
+        (4, 'Добре'),
+        (5, 'Відмінно'),
+    ]
+
+    user = models.ForeignKey(
+        CustomUser,
+        on_delete=models.CASCADE,
+        verbose_name='Користувачі',
+        related_name='user_review'
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        verbose_name='Товари',
+        related_name='review_product'
+    )
+
+    rating = models.PositiveSmallIntegerField(choices=CHOICES_RATING, verbose_name='Рейтинг')
+    text = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ['-created_at',]
+        unique_together = ['user', 'product']
+        verbose_name = 'Відгук'
+        verbose_name_plural = 'Відгуки'
+
+    def __str__(self):
+        return f'Користувач {self.user}\nТовар {self.product}\nРейтинг {self.rating}'
