0c4ce6bb9a8c3c20362c3b77077f18b0c315320b
Vlad <bahlaiv170307@gmail.com>
2026-07-08 15:07:30 +0300

    feat: serializer for catalog

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Cart, CartItem
+from .serializers import CartSerializer, CartItemSerializer
 
-# Create your views here.
+class CartViewSet(viewsets.ModelViewSet):
+    serializer_class = CartSerializer
+
+    def get_queryset(self):
+        return Cart.objects.filter(user=self.request.user)
+
+
+class CartItemViewSet(viewsets.ModelViewSet):
+    serializer_class = CartItemSerializer
+
+    def get_queryset(self):
+        return CartItem.objects.filter(cart__user=self.request.user)
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import MainCategory, SubCategory, Brand, Product
+from .serializers import MainCategorySerializer, SubCategorySerializer, BrandSerializer, ProductSerializer
 
-# Create your views here.
+
+class MainCategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = MainCategorySerializer
+    queryset = MainCategory.objects.all()
+    
+
+class SubCategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = SubCategorySerializer
+    queryset = SubCategory.objects.all()
+    
+
+class BrandViewSet(viewsets.ModelViewSet):
+    serializer_class = BrandSerializer
+    queryset = Brand.objects.all()
+    
+
+class ProductViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
