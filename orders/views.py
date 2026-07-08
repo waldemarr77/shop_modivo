134c5cdb415e59c41f2ca0326208d1fe9f511568
Vlad <bahlaiv170307@gmail.com>
2026-07-08 15:23:05 +0300

    feat: views for orders

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Order, OrderItem
+from .serializers import OrderSerializer, OrderItemSerializer
 
-# Create your views here.
+
+class OrderViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
+    
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    serializer_class = OrderItemSerializer
+
+    def get_queryset(self):
+        return OrderItem.objects.filter(order__user=self.request.user)
+
