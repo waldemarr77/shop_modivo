e27504586c63723d2f4d69f8153493a492940a64
Vlad <bahlaiv170307@gmail.com>
2026-07-08 15:12:26 +0300

    feat: serializer for inventory

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Size, ProductVariant
+from .serializers import SizeSerializer, ProductVariantSerializer
 
-# Create your views here.
+
+class SizeViewSet(viewsets.ModelViewSet):
+    serializer_class = SizeSerializer
+    queryset = Size.objects.all()
+
+
+class ProductVariantViewSet(viewsets.ModelViewSet):
+    serializer_class = ProductVariantSerializer
+    queryset = ProductVariant.objects.all()
