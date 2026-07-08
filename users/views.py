3c7559cf0e7e83d1f380f25fabc23b15e9964746
Vlad <bahlaiv170307@gmail.com>
2026-07-08 15:43:07 +0300

    feat: views for users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import generics
+from .models import CustomUser
+from .serializers import CustomUserSerializer, RegisterSerializer
 
-# Create your views here.
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    serializer_class = CustomUserSerializer
+
+    def get_queryset(self):
+        return CustomUser.objects.filter(username=self.request.user.username)
+    
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    
