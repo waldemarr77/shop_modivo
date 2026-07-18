7a674e39d97410e88fb1cc7240be183e345e137e
Vlad <bahlaiv170307@gmail.com>
2026-07-18 14:08:56 +0300

    feat: add catalog frontend page

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -33,4 +34,5 @@ urlpatterns = [
     path('api/users/', include('users.urls')),
 
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', lambda request: render(request, 'catalog.html'), name='catalog'),
 ]
